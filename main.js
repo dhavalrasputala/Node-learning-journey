1640a6c983559faf128c0f228516d5df606b676e
TeCH_enTHusIAst97 <59308013+dhavalrasputala@users.noreply.github.com>
2026-01-18 12:03:54 +0530

    learning Dynamic Routing in Node

--- main.js
+++ main.js
@@ -1,32 +1,57 @@
-const { STATUS_CODES } = require("http");
-
-const routeResponseMap = {
-    "/info":"<h1>Info Page</h1>",
-    "/contact":"<h1>Contact Us</h1>",
-    "/about":"<h1>Learn More About US</h1>",
-    "/hello":"<h1>Say Hello by Emailing us</h1>",
-    "/error":"<h1>Sorry the Page you are looking for is not here</h1>",
-}
-
-const port = 3000;
-
-http = require("http")
-
+const port = 2006
+http = require("http");
 httpStatus = require("http-status-codes");
+const sendErrorResponse = res => {
+    if (res.headersSent) return;
 
-app = http.createServer((req,res)=>{
-
-    res.writeHead(200,{
-       "content-Type":"text/html"
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
     });
-
-    if(routeResponseMap[req.url]) {
-
-        res.end(routeResponseMap[req.url]);
-    } else {
-        res.end("<h1>Welcome</h1>");
+    res.end("<h1>FILE NOT FOUND</h1>");
+};
+http.createServer((req,res) => {
+    let url = req.url;
+    if (url.indexOf(".html")!==-1){
+        res.writeHead(httpStatus.OK,{
+            "Content-Type":"text/html"
+        });
+        customReadFile(`./view${url}`,res);
     }
-});
+    else if (url.indexOf(".js")!==-1){
+        res.writeHead(httpStatus.OK,{
+            "Content-Type":"text/javascript"
+        });
+        customReadFile(`./public/js${url}`,res);
+    }
+    else if (url.indexOf(".css")!==-1){
+        res.writeHead(httpStatus.OK,{
+            "Content-Type":"text/css"
+        });
+        customReadFile(`./public/css${url}`,res);
+    }
+    else if (url.indexOf(".png")!==-1){
+        res.writeHead(httpStatus.OK,{
+            "Content-Type":"image/png"
+        });
+        customReadFile(`./public/images${url}`,res);
+    }
+    else {
+        return sendErrorResponse(res);
+    }
+}).listen(port);
 
-app.listen(port)
-console.log(`The server running at port number:${port}`)
+console.log(`The	server	is	listening	on	port	number:	${port}`)
+
+const customReadFile = (file_path, res) => {
+    if (!fs.existsSync(file_path)) {
+        return sendErrorResponse(res);
+    }
+
+    fs.readFile(file_path, (error, data) => {
+        if (error) {
+            console.log(error);
+            return sendErrorResponse(res);
+        }
+        res.end(data);
+    });
+};
